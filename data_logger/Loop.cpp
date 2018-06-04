9ac70c8f77e7ec67fd9376b2e819c1bf4659dd20
Mikko <mikko.tikkakoski@gmail.com>
2018-06-04 21:41:25 +0300

    data_logger prints hello world. lcd_i2c using 4 row unit

--- data_logger/Loop.cpp
+++ data_logger/Loop.cpp
@@ -5,18 +5,15 @@ void loop()
 {
   // make a string for assembling the data to log:
   String dataString = "";
-
-  // read three sensors and append to the string:
-  for (int analogPin = 0; analogPin < 3; analogPin++) {
-    //int sensor = analogRead(analogPin);
-      int sensor = (analogPin * 100);
-    dataString += String(sensor);
-    dataString += ",";
-  }
-  dataString += "\n";
+  dataString += "Hello world";
 
   // open the file. note that only one file can be open at a time,
   // so you have to close this one before opening another.
+    if (!SD.mkdir("LOGS/"))
+    {
+      Serial.println("mkdir failed");
+      return;
+    }
   File dataFile = SD.open("datalog.txt", FILE_WRITE);
 
   // if the file is available, write to it:
--- lcd_i2c/Loop.cpp
+++ lcd_i2c/Loop.cpp
@@ -1,7 +1,24 @@
 
 #include "Arduino.h"
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+extern LiquidCrystal_I2C lcd;
 
+unsigned long counter = 0;
 void loop()
 {
-
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Hello, world!");
+  lcd.setCursor(0,1);
+  lcd.print(counter);
+  counter++;
+  lcd.setCursor(0,2);
+  const unsigned long mils = millis();
+  const unsigned long mils2 = millis();
+  lcd.print(mils);
+  lcd.print(" ");
+  lcd.print(mils2);
+  delay(1000);
 }
+
