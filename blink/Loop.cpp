0ba036a4140511b0e544f921c53a6f200c665cd7
Mikko Tikkakoski <mikko.tikkakoski@gmail.com>
2019-05-13 21:31:31 +0300

    could not use LED_BUILTIN with esp8266-07 as generic esp8266 module. It crashed.

--- blink/Loop.cpp
+++ blink/Loop.cpp
@@ -1,10 +1,13 @@
 
 #include "Arduino.h"
+#include "Led.h"
 
 void loop()
 {
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    Serial.println("HIGH");
+    digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
     delay(1000);                       // wait for a second
-    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+    Serial.println("LOW");
+    digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
     delay(1000);                       // wait for a second
 }
--- blink/Setup.cpp
+++ blink/Setup.cpp
@@ -1,13 +1,8 @@
 #include "Arduino.h"
+#include "Led.h"
 
-static const int ledPin = 2;
 void setup()
 {
-  pinMode(2, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(12, OUTPUT);
-  pinMode(13, OUTPUT);
-  pinMode(14, OUTPUT);
-  pinMode(16, OUTPUT);
+  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
 }
