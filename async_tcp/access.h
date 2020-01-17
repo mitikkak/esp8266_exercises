e3ebda5ba2b0770f7e155f2224fd7b2ca1d4bcbe
Mikko Tikkakoski <mikko.tikkakoski@nokia.com>
2020-01-17 13:51:06 +0200

    added async_tcp, untested

---
+++ async_tcp/access.h
@@ -0,0 +1,8 @@
+
+#define SSID "ESP-TEST"
+#define PASSWORD "123456789"
+
+#define SERVER_HOST_NAME "esp_server"
+
+#define TCP_PORT 7050
+#define DNS_PORT 53
---
+++ async_tcp/access_point.h
@@ -0,0 +1,3 @@
+
+const char* ssid = "async_udp_ap";
+const char* password = "abba_acdc";
