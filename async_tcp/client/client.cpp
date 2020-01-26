5657ff4dcf25588fac5647467d53842a48d6f7fe
Mikko Tikkakoski <mikko.tikkakoski@gmail.com>
2020-01-26 15:10:33 +0200

    espwroom32 working with async_tcp

--- async_tcp/client/client.cpp
+++ async_tcp/client/client.cpp
@@ -6,41 +6,55 @@
 #include "ESPAsyncTCP.h"
 #endif
 
-extern "C" {
-#include <osapi.h>
-#include <os_type.h>
-}
+//extern "C" {
+//#include <osapi.h>
+//#include <os_type.h>
+//}
 
 #include "access.h"
+#include "LoopLogger.h"
 
-static os_timer_t intervalTimer;
+//static os_timer_t intervalTimer;
 
-static void replyToServer(void* arg) {
+int counter{0};
+static void sendToServer(void* arg) {
 	AsyncClient* client = reinterpret_cast<AsyncClient*>(arg);
 
 	// send reply
+	Serial.printf("[%lu] space: %lu canSend: %u \n\r", millis(), client->space(), client->canSend());
 	if (client->space() > 32 && client->canSend()) {
-		char message[32];
-		sprintf(message, "this is from %s", WiFi.localIP().toString().c_str());
-		client->add(message, strlen(message));
+		//char message[32];
+		//sprintf(message, "%s", WiFi.localIP().toString().c_str());
+	    String message(counter);
+		client->add(message.c_str(), message.length());
 		client->send();
+		counter++;
 	}
 }
 
 /* event callbacks */
 static void handleData(void* arg, AsyncClient* client, void *data, size_t len) {
-	Serial.printf("\n data received from %s \n", client->remoteIP().toString().c_str());
+	Serial.printf("[%lu] data received from %s \n\r", millis(), client->remoteIP().toString().c_str());
 	Serial.write((uint8_t*)data, len);
 
-	os_timer_arm(&intervalTimer, 2000, true); // schedule for reply to server at next 2s
+	//os_timer_arm(&intervalTimer, 2000, true); // schedule for reply to server at next 2s
+}
+static void handleAck(void* arg, AsyncClient* client, size_t len, uint32_t time)
+{
+    Serial.printf("[%lu] handleAck \n\r", millis());
+}
+
+static void handleAckTimeout(void* arg, AsyncClient* client, uint32_t time)
+{
+    Serial.printf("[%lu] handleAckTimeout \n\r", millis());
 }
 
 void onConnect(void* arg, AsyncClient* client) {
-	Serial.printf("\n client has been connected to %s on port %d \n", SERVER_HOST_NAME, TCP_PORT);
-	replyToServer(client);
+	Serial.printf("[%lu] client has been connected to %s on port %d \n\r", millis(), SERVER_HOST_NAME, TCP_PORT);
+	//sendToServer(client);
 }
 
-
+AsyncClient* client{nullptr};
 void setup() {
 	Serial.begin(115200);
 	delay(20);
@@ -53,15 +67,30 @@ void setup() {
 		delay(500);
 	}
 
-	AsyncClient* client = new AsyncClient;
+	client = new AsyncClient;
 	client->onData(&handleData, client);
 	client->onConnect(&onConnect, client);
 	client->connect(SERVER_HOST_NAME, TCP_PORT);
+	client->onAck(&handleAck, client);
+	client->onTimeout(&handleAckTimeout, client);
+	Serial.printf("client [%u][%u][%u][%u] \n\r", client->getMss(), client->getRxTimeout(), client->getAckTimeout(), client->getNoDelay());
 
-	os_timer_disarm(&intervalTimer);
-	os_timer_setfn(&intervalTimer, &replyToServer, client);
+//	os_timer_disarm(&intervalTimer);
+//	os_timer_setfn(&intervalTimer, &sendToServer, client);
 }
 
-void loop() {
-
+LoopLogger loopLogger(5000);
+unsigned long prevTimeSent{0};
+bool timeToSend()
+{
+    return (millis() - prevTimeSent) >= 1000;
+}
+void loop()
+{
+    loopLogger.step();
+    if (timeToSend())
+    {
+        sendToServer(client);
+        prevTimeSent = millis();
+    }
 }
--- async_tcp/server/server.cpp
+++ async_tcp/server/server.cpp
@@ -1,9 +1,15 @@
-#include <ESP8266WiFi.h>
-#include <ESPAsyncTCP.h>
+#ifdef ESP32
+#include "WiFi.h"
+#include "AsyncTCP.h"
+#else
+#include "ESP8266WiFi.h"
+#include "ESPAsyncTCP.h"
+#endif
 #include <DNSServer.h>
 #include <vector>
 
 #include "access.h"
+#include "LoopLogger.h"
 
 static DNSServer DNS;
 
@@ -11,13 +17,17 @@ static std::vector<AsyncClient*> clients; // a list to hold all clients
 
  /* clients events */
 static void handleError(void* arg, AsyncClient* client, int8_t error) {
-	Serial.printf("\n connection error %s from client %s \n", client->errorToString(error), client->remoteIP().toString().c_str());
+	Serial.printf("[%lu] connection error %s from client %s \n\r", millis(), client->errorToString(error), client->remoteIP().toString().c_str());
 }
 
+unsigned int expected = 0;
 static void handleData(void* arg, AsyncClient* client, void *data, size_t len) {
-	Serial.printf("\n data received from client %s \n", client->remoteIP().toString().c_str());
+	Serial.printf("[%lu] data received from client %s \n\r", millis(), client->remoteIP().toString().c_str());
 	Serial.write((uint8_t*)data, len);
+	Serial.printf(" expected: %u \n\r", expected);
+	expected++;
 
+#if 0
 	// reply to client
 	if (client->space() > 32 && client->canSend()) {
 		char reply[32];
@@ -25,20 +35,21 @@ static void handleData(void* arg, AsyncClient* client, void *data, size_t len) {
 		client->add(reply, strlen(reply));
 		client->send();
 	}
+#endif
 }
 
 static void handleDisconnect(void* arg, AsyncClient* client) {
-	Serial.printf("\n client %s disconnected \n", client->remoteIP().toString().c_str());
+	Serial.printf("[%lu] client %s disconnected \n\r", millis(), client->remoteIP().toString().c_str());
 }
 
 static void handleTimeOut(void* arg, AsyncClient* client, uint32_t time) {
-	Serial.printf("\n client ACK timeout ip: %s \n", client->remoteIP().toString().c_str());
+	Serial.printf("[%lu] client ACK timeout ip: %s \n\r", millis(), client->remoteIP().toString().c_str());
 }
 
 
 /* server events */
 static void handleNewClient(void* arg, AsyncClient* client) {
-	Serial.printf("\n new client has been connected to server, ip: %s", client->remoteIP().toString().c_str());
+	Serial.printf("[%lu] new client has been connected to server, ip: %s\n\r", millis(), client->remoteIP().toString().c_str());
 
 	// add to list
 	clients.push_back(client);
@@ -61,13 +72,16 @@ void setup() {
 
 	// start dns server
 	if (!DNS.start(DNS_PORT, SERVER_HOST_NAME, WiFi.softAPIP()))
-		Serial.printf("\n failed to start dns service \n");
+		Serial.printf("failed to start dns service \n\r");
 
 	AsyncServer* server = new AsyncServer(TCP_PORT); // start listening on tcp port 7050
 	server->onClient(&handleNewClient, server);
+	server->setNoDelay(true);
 	server->begin();
 }
 
+LoopLogger loopLogger(5000);
 void loop() {
 	DNS.processNextRequest();
+	loopLogger.step();
 }
